359a4cea6dbcf9adf07e692307ec4d7406f43460
Ernesto <ernestov@cetys.edu.mx>
2020-05-18 06:06:45 -0700

    first commit

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Observable, of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { UserService } from './user.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  user$: Observable<any>;
+
+  constructor(private afa: AngularFireAuth,
+              private userService: UserService) {
+    this.user$ = this.afa.authState.pipe(
+      switchMap(user => {
+        if (user) {
+          return this.userService.getUser(user.uid);
+        } else {
+          return of(null);
+        }
+      })
+    );
+
+  }
+
+  login(email: string, password: string): Promise<any> {
+    return this.afa.auth.signInWithEmailAndPassword(email, password);
+  }
+
+  signup(email: string, password: string): Promise<any> {
+    return this.afa.auth.createUserWithEmailAndPassword(email, password);
+  }
+
+  logout(): Promise<void> {
+    return this.afa.auth.signOut();
+  }
+}
